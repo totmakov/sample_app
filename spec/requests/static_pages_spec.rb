4451303144be44a98efa3838fd1bd2be0a840f9c
Andrew Totmakov <andrew.totmakov@outlook.com>
2015-02-23 02:00:32 +0300

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,12 +7,22 @@ describe "StaticPages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+
+    it "should have right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+    end
   end
 
   describe "Help page" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
+    end
+
+    it "should have right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
     end
   end
 
@@ -21,5 +31,12 @@ describe "StaticPages" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
+
+    it "should have right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
+    end
   end
+
+
 end
